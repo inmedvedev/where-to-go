5b87417ebf0bc1c2de3428d51296ecb57108f5bd
turim0 <ivan_medvedev_1996@mail.ru>
2022-01-27 01:06:17 +0300

    image display

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['show_image']
+    fields = ('image', 'show_image', 'position')
+
+    def show_image(self, instance):
+        multiplier = 200/instance.image.height
+        return format_html(
+            '<img src="{url}" width="{width}" height={height} />',
+            url=instance.image.url,
+            width=int(multiplier * instance.image.width),
+            height=int(multiplier * instance.image.height)
+        )
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
